03d74c3028d933cfc9883fe9512c86e93f6e214e
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-02-11 12:14:46 -0500

    fixed fonts

---
+++ script.js
@@ -0,0 +1,60 @@
+function toggleVoyages() {
+  var voyagesDiv = document.getElementById('voyages');
+	var displaySettingVoyages = voyagesDiv.style.display;
+	var factsDiv = document.getElementById('facts');
+	var displaySettingFacts = factsDiv.style.display;
+	var signalDiv = document.getElementById('signal');
+	var displaySettingSignal = signalDiv.style.display;
+
+	if (displaySettingVoyages == 'block') {
+	  voyagesDiv.style.display = 'none';
+	  }
+	else if (displaySettingFacts == 'block' || displaySettingSignal == 'block'){
+	    factsDiv.style.display = 'none';
+		signalDiv.style.display = 'none';
+		voyagesDiv.style.display = 'block';
+	  }
+	else {
+	  voyagesDiv.style.display = 'block';
+	}
+};
+function toggleFacts() {
+  var voyagesDiv = document.getElementById('voyages');
+	var displaySettingVoyages = voyagesDiv.style.display;
+	var factsDiv = document.getElementById('facts');
+	var displaySettingFacts = factsDiv.style.display;
+	var signalDiv = document.getElementById('signal');
+	var displaySettingSignal = signalDiv.style.display;
+
+	if (displaySettingFacts == 'block') {
+	  factsDiv.style.display = 'none';
+	  }
+	  else if (displaySettingVoyages == 'block' || displaySettingSignal == 'block'){
+	    voyagesDiv.style.display = 'none';
+		signalDiv.style.display = 'none';
+		factsDiv.style.display = 'block';
+	  }
+	  else {
+	    factsDiv.style.display = 'block';
+	}
+};
+function toggleSignal() {
+  var voyagesDiv = document.getElementById('voyages');
+	var displaySettingVoyages = voyagesDiv.style.display;
+	var factsDiv = document.getElementById('facts');
+	var displaySettingFacts = factsDiv.style.display;
+	var signalDiv = document.getElementById('signal');
+	var displaySettingSignal = signalDiv.style.display;
+
+	if (displaySettingSignal == 'block') {
+	  signalDiv.style.display = 'none';
+	  }
+	  else if (displaySettingFacts == 'block' || displaySettingVoyages == 'block'){
+	    factsDiv.style.display = 'none';
+		voyagesDiv.style.display = 'none';
+		signalDiv.style.display = 'block';
+	  }
+	  else {
+	    signalDiv.style.display = 'block';
+	}
+};
